39b5c05b5bb3f6e7d69074feb197c0b873610c68
Playerleleo <leonardogomesdossantos123@gmail.com>
2025-05-07 20:33:54 -0300

    Resolve Query ListOrders GraphQL

--- internal/infra/graph/schema.resolvers.go
+++ internal/infra/graph/schema.resolvers.go
@@ -30,7 +30,34 @@ func (r *mutationResolver) CreateOrder(ctx context.Context, input *model.OrderIn
 	}, nil
 }
 
+// ListOrders is the resolver for the listOrders field.
+func (r *queryResolver) ListOrders(ctx context.Context) ([]*model.Order, error) {
+	orders, err := r.ListOrdersUseCase.Execute()
+	if err != nil {
+		return nil, err
+	}
+
+	var ordersResponse []*model.Order
+	for _, order := range orders {
+		ordersResponse = append(ordersResponse, &model.Order{
+			ID:         order.ID,
+			Price:      float64(order.Price),
+			Tax:        float64(order.Tax),
+			FinalPrice: float64(order.FinalPrice),
+		})
+	}
+	return ordersResponse, nil
+}
+
 // Mutation returns MutationResolver implementation.
 func (r *Resolver) Mutation() MutationResolver { return &mutationResolver{r} }
 
+// Query returns QueryResolver implementation.
+func (r *Resolver) Query() QueryResolver { return &queryResolver{r} }
+
 type mutationResolver struct{ *Resolver }
+type queryResolver struct{ *Resolver }
+
+type QueryResolver interface {
+	ListOrders(ctx context.Context) ([]*model.Order, error)
+}
